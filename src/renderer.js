c6d119d2948dc95c12d3eb12570b58a7113ecdd8
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-10-21 11:47:37 -0400

    Basic template working!

--- src/renderer.js
+++ src/renderer.js
@@ -26,6 +26,51 @@
  * ```
  */
 
-import './index.css';
+// import './index.css';
 
-console.log('👋 This message is being logged by "renderer.js", included via Vite');
+// console.log('👋 This message is being logged by "renderer.js", included via Vite');
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './renderer/App.vue'
+import router from './renderer/router'
+import { useMainStore } from './renderer/stores/main.js'
+
+import './renderer/css/main.css'
+
+// Init Pinia
+const pinia = createPinia()
+
+// Create Vue app
+createApp(App).use(router).use(pinia).mount('#app')
+
+// Init main store
+const mainStore = useMainStore(pinia)
+
+// Fetch sample data
+mainStore.fetchSampleClients()
+mainStore.fetchSampleHistory()
+
+// Dark mode
+// Uncomment, if you'd like to restore persisted darkMode setting, or use `prefers-color-scheme: dark`. Make sure to uncomment localStorage block in src/stores/darkMode.js
+// import { useDarkModeStore } from './stores/darkMode'
+
+// const darkModeStore = useDarkModeStore(pinia)
+
+// if (
+//   (!localStorage['darkMode'] && window.matchMedia('(prefers-color-scheme: dark)').matches) ||
+//   localStorage['darkMode'] === '1'
+// ) {
+//   darkModeStore.set(true)
+// }
+
+// Default title tag
+const defaultDocumentTitle = 'Admin One Vue 3 Tailwind'
+
+// Set document title from route meta
+router.afterEach((to) => {
+    document.title = to.meta?.title
+        ? `${to.meta.title} — ${defaultDocumentTitle}`
+        : defaultDocumentTitle
+})
