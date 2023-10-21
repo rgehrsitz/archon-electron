1a7fd8650bed94f3b7b762a295ed9dc99ae3e3b2
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-10-21 11:29:20 -0400

    Most admin-one template files brought over

--- vite.renderer.config.mjs
+++ vite.renderer.config.mjs
@@ -1,4 +1,15 @@
-import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from "node:url";
 
-// https://vitejs.dev/config
-export default defineConfig({});
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: "/admin-one-vue-tailwind/",
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+});
