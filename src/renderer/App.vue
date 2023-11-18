334b17584a2cb5de9891efc745e408d1b1dade48
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-11-18 15:41:49 -0500

    Tried to switch to useRoute

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,7 +1,31 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import StyleView from '@/views/StyleView.vue';
+import HomeView from '@/views/HomeView.vue';
+import WelcomeView from '@/views/WelcomeView.vue';
+import EquipmentView from '@/views/EquipmentView.vue';
+import SnapshotsView from './views/SnapshotsView.vue';
+import DiffView from './views/DiffView.vue';
+import SettingsView from './views/SettingsView.vue';
+// ... import other views as needed
+
+const route = useRoute();
+
+const routeComponents = {
+  '/': StyleView,
+  '/welcome': WelcomeView,
+  '/dashboard': HomeView,
+  '/equipment': EquipmentView,
+  '/snapshots': SnapshotsView,
+  '/diffview': DiffView,
+  '/settings': SettingsView,
+  // ... map other routes to components
+};
+
+const currentComponent = computed(() => routeComponents[route.path]);
 </script>
 
 <template>
-  <RouterView />
+  <component :is="currentComponent" />
 </template>
